5ef6e7ec925e2340aa765253c6fd9bc665fcccd5
Joanne <joannechanliu@gmail.com>
2024-02-23 01:17:08 -0600

    Worked on chessboard puzzle more

--- chessboard.playground/Contents.swift
+++ chessboard.playground/Contents.swift
@@ -1,19 +1,22 @@
 // number of rows
-var n:Int = 8
+var n:Int = 4
 
 // creates n by n board initialized to 0s
 var board = Array(repeating: Array(repeating: 0, count: n), count: n)
 
 // print board
+// for loop needs space around ..<
+print("original board:")
 for i in 0 ..< n {
     print(board[i])
 }
 
-var row:Int = 0
-var col:Int = 0
+print()
+
 
 // how to pass in 2D array for functions?
-func isValid(_board :[[Int]], _ n:Int, _ row: Int, _ col: Int) -> Bool {
+// function returns whether putting a queen in the passed in location will still make the board valid
+func isValid(_ board :[[Int]], _ n:Int, _ row: Int, _ col: Int) -> Bool {
     // check if there is already one placed in row
     for i in 0 ..< n {
         if(board[row][i] == 1) {
@@ -47,4 +50,31 @@ func isValid(_board :[[Int]], _ n:Int, _ row: Int, _ col: Int) -> Bool {
 }
 
 // loop through board to see if valid
+var row:Int = 0
+var col:Int = 0
 
+// array of size n to store the column for each row where the pieces r placed
+var columns = Array(repeating: 0, count: n)
+
+while row < n {
+    for j in columns[row] ..< n {
+        if isValid(board, n, row, j) {
+            board[row][j] = 1
+            columns[row] = j // update column position
+            // ++ does not exist in Swift
+            row += 1
+            break
+        }
+        // if on the last iteration and putting it in the last col doesn't work
+        if (j == n - 1) {
+            row -= 1
+            board[row][columns[row]] = 0
+            columns[row] += 1
+        }
+    }
+    print(row, columns[row])
+}
+
+for i in 0 ..< n {
+    print(board[i])
+}
