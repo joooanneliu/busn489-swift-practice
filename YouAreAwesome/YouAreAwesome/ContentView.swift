775dbfdcd4561ef3c39ab8fb9419c3b91e69d2fc
Joanne <joannechanliu@gmail.com>
2024-03-04 23:33:00 -0600

    Completed swift tutorial up until Ch 1.4

--- YouAreAwesome/YouAreAwesome/ContentView.swift
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // will not be destroyed when a view is recreated
+    // define variables before body view
+   @State private var messageString = ""
+    
     var body: some View {
+        // allows variables to be changed in views
+        
+        // without vstack, the image and text will go on separate views
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "swift")
+                // Add modifers tha return image before modifiers to view
+                // i.e. resizable before foregroundstyle
+                .resizable()
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("You Are Awesome")
+                .foregroundStyle(.orange)
+                .scaledToFit()
+                .padding(.bottom)
+            Text(messageString)
+                .font(.title2)
+            
+            HStack {
+                Button("Awesome") {
+                    // this is the action perforned when button is pressed
+                    // views are structs, which are valuetypes and immutable
+                    // therefore, need to make messageString a state property
+                    messageString = "You Are Awesome!!"
+                }.buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }.buttonStyle(.borderedProminent)
+            }
+            
+           
+        
         }
         .padding()
     }
